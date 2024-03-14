4e475f955d546c16b4c112cb6ae57e8afbfb9e4b
Boston Phillips-Lepaio <bostonpl20@gmail.com>
2024-03-14 16:28:45 +1300

    Integrated API

--- client/apiClient.ts
+++ client/apiClient.ts
@@ -1,6 +1,12 @@
 import request from 'superagent'
+import { Root } from './models/quotes'
 
 export async function getGreeting(): Promise<string> {
   const res = await request.get('/api/v1/greeting')
   return res.body.greeting
+}
+
+export async function getRandomQuote(): Promise<Root> {
+  const res = await request.get(`https://api.quotable.io/random`)
+  return res.body.result
 }
